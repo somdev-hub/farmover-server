902c27ce4933e5281e0bf63b72b09564732894cf
Somdev Behera <64832169+somdev-hub@users.noreply.github.com>
2024-08-26 11:40:36 +0530

    removed backend support for spring ai

--- src/main/java/com/farmover/server/farmover/controllers/FarmAiController.java
+++ src/main/java/com/farmover/server/farmover/controllers/FarmAiController.java
@@ -1,13 +1,10 @@
 package com.farmover.server.farmover.controllers;
 
-import java.util.Map;
-
-import org.springframework.ai.vertexai.palm2.VertexAiPaLm2ChatModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,17 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class FarmAiController {
 
-    private VertexAiPaLm2ChatModel vertexAiPaLm2ChatModel;
+    @Value("${spring.ai.vertex.ai.api-key}")
+    private String apiKey;
 
-    public FarmAiController(VertexAiPaLm2ChatModel chatModel) {
-        this.vertexAiPaLm2ChatModel = chatModel;
-    }
-
-    @PostMapping("/chat")
-    public ResponseEntity<Map<String, String>> getResponse(@RequestBody String prompt) {
-        Map<String, String> generation = Map.of("generation", vertexAiPaLm2ChatModel.call(prompt));
-
-        return new ResponseEntity<Map<String, String>>(generation, HttpStatus.OK);
+    @GetMapping("/key")
+    public ResponseEntity<String> getApiKey() {
+        return new ResponseEntity<String>(apiKey, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/farmover/server/farmover/services/impl/ProductionServiceImpl.java
+++ src/main/java/com/farmover/server/farmover/services/impl/ProductionServiceImpl.java
@@ -4,7 +4,6 @@ import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
